9ef03c77a8d8713cecbd5dd1cfc0e9a908449a9a
Divya <64250511+Divyashivakumar10@users.noreply.github.com>
2020-12-04 13:04:03 -0600

    Add files via upload

---
+++ src/test/java/org/apache/commons/mail/testcase1.java
@@ -0,0 +1,31 @@
+package org.apache.commons.mail;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import junit.framework.TestCase;
+
+public class testcase1 extends TestCase{
+	Email testEmail;
+	public void setUp() {
+		testEmail = new SimpleEmail();
+	}
+
+	public void testAddCc() throws EmailException {
+		System.out.println("This is test for Cc - lon___");
+		testEmail.addCc("a@b.com");
+		assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
+	}
+
+	
+    	public void testAddCcMultiple() throws EmailException {
+        	System.out.println("Test to add multiple CC addresses - lon___"); 
+		String [] testEmails = {"a@b.com", "b@c.com"};
+        	testEmail.addCc(testEmails);
+        	assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
+        	assertEquals("b@c.com", testEmail.getCcAddresses().get(1).toString());
+    	}
+
+
+}
---
+++ src/test/java/org/apache/commons/mail/testcase2.java
@@ -0,0 +1,29 @@
+package org.apache.commons.mail;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import junit.framework.TestCase;
+
+public class testcase2 extends TestCase{
+	Email testEmail;
+	public void setUp() {
+		testEmail = new SimpleEmail();
+	}
+	public void testAddCc() throws EmailException {
+		System.out.println("Test to add CC address - lon___");
+		testEmail.addCc("a@b.com");
+		assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
+	}  
+	
+	
+    	public void testAddBccMultiple() throws EmailException {
+		System.out.println("Test to add multiple BCC addresses - lon___"); 
+        	String [] testEmails = {"a@b.com", "b@c.com"}; 
+        	testEmail.addBcc(testEmails);
+        	assertEquals("a@b.com", testEmail.getBccAddresses().get(0).toString()); 
+        	assertEquals("b@c.com", testEmail.getBccAddresses().get(1).toString()); 
+    	}
+
+}
