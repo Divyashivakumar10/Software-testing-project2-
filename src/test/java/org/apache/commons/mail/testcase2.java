7c7b80cf25be7bd74343ae96b4fc64d08b569869
Divya <64250511+Divyashivakumar10@users.noreply.github.com>
2020-12-04 19:17:43 -0600

    Update testcase2.java

--- src/test/java/org/apache/commons/mail/testcase2.java
+++ src/test/java/org/apache/commons/mail/testcase2.java
@@ -12,14 +12,14 @@ public class testcase2 extends TestCase{
 		testEmail = new SimpleEmail();
 	}
 	public void testAddCc() throws EmailException {
-		System.out.println("Test to add CC address - lon___");
+		System.out.println("Test to add CC address ID - Lon");
 		testEmail.addCc("a@b.com");
 		assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
 	}  
 	
 	
     	public void testAddBccMultiple() throws EmailException {
-		System.out.println("Test to add multiple BCC addresses - lon___"); 
+		System.out.println("Test to add multiple BCC addresses ID - Lon"); 
         	String [] testEmails = {"a@b.com", "b@c.com"}; 
         	testEmail.addBcc(testEmails);
         	assertEquals("a@b.com", testEmail.getBccAddresses().get(0).toString()); 
